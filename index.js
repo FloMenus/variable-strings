6a676d8ba287f67c54e5cafcd7829fc481c406a3
FloMenus <florent.mns@gmail.com>
2022-06-20 13:35:14 +0200

    added js file

---
+++ index.js
@@ -0,0 +1,46 @@
+// 1- Hello World
+
+console.log ("Hello World");
+
+//2- String 
+
+//const sentence = "My name is Florent";
+//console.log(sentence);
+
+//3- Concaténation
+
+//const name ="Florent";
+//console.log (`Nice to meet you ${name}`);
+
+//4- String Lenght
+
+//const sentence ="I'm a new text !";
+//console.log (sentence.length);
+
+//5- Replace 
+
+//const food="croissant is meh";
+//console.log (food.replace("meh","so good"));
+
+//6- Up and down 
+
+//const basic = "This is Cool";
+//const basicUp = basic.toUpperCase();
+//const basicDown = basic.toLowerCase();
+
+//console.log (`${basic} ${basicUp} ${basicDown}`)
+
+//7 - Split
+
+//const word ="banana"
+//const letters = word.split("")
+
+//console.log (letters)
+
+//8 - Template
+
+const age ="23";
+const template=`I'm ${age} years old`
+
+console.log (template);
+
